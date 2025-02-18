d5e0ee87315c5848113f51c5c4cc2cfe7d5cd445
Tonny Silva <junior.silva.bernardino@gmail.com>
2025-02-17 23:24:16 -0300

    initial setup of About structure and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import About from "./components/About";
 
 const App = () => {
   return (
     <>
       <Navbar/>
       <Header/>
+      <About/>
     </>
   );
 };
