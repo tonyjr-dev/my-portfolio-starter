b115f396d1d061cc40658854fab0fc5cb4bca963
Tonny Silva <junior.silva.bernardino@gmail.com>
2025-02-18 21:36:23 -0300

    initial setup of project and style

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import About from "./components/About";
 import Services from "./components/Services";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <About/>
       <Services/>
       <Skills/>
+      <Projects/>
     </>
   );
 };
