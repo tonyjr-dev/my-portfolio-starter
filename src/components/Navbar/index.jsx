baeeda5ef4def6aeabc7d7842bc4044443cd55e4
Tonny Silva <junior.silva.bernardino@gmail.com>
2025-02-22 17:17:18 -0300

    settings Menu in Jsx, Reusing Css codes from index.css

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,9 +3,13 @@ import Logo from "../../assets/Logo"
 import { FaTimes } from "react-icons/fa";
 import { menu } from "../../data";
 import { Link } from "react-scroll";
-import { FaArrowUpRightFromSquare } from "react-icons/fa6";
+import { FaArrowUpRightFromSquare, FaBarsStaggered
+} from "react-icons/fa6";
+import { useState } from "react";
 
 const Navbar = () => {
+    const [showSidebar,setShowSidebar] = useState(false);
+    console.log("show Sidebar:",showSidebar)
     return (
         <nav className="card flex__center navbar">
             <div className="flex__center log">
@@ -37,10 +41,11 @@ const Navbar = () => {
                 </div>
             </aside>
             <div className="flex__container buttons__wapper">
-                <Link to="contact"> 
+                <Link to="contact" className="btn flex__center hire__btn"> 
                 Hire Me <div className="flex__center icon"></div>
                  <FaArrowUpRightFromSquare/>
                 </Link>
+                    <FaBarsStaggered className="menu" />
             </div>
         </nav>
     )
